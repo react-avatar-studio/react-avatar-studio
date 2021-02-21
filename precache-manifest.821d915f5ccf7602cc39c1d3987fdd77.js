c8990bbe41648920275112b2fa7b562859870046
Daniel Padrino <daniel.padrino@inferstat.com>
2021-02-21 11:14:46 -0300

    Updates

---
+++ precache-manifest.821d915f5ccf7602cc39c1d3987fdd77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfb6998c3dfc312f8a0db406387ef9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e9d6f93262d0f8241a3",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "3c5a9e3d78a5a4431500",
+    "url": "./static/js/2.157e24f9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.157e24f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9d6f93262d0f8241a3",
+    "url": "./static/js/main.14e7b2f0.chunk.js"
+  },
+  {
+    "revision": "98ebc5229183a45e7c44",
+    "url": "./static/js/runtime-main.0bc7c68f.js"
+  }
+]);
